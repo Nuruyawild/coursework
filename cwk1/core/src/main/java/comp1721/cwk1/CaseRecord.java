d524ecc74a525a8756944b84d33f7be38bb68ffa
nuruyawild <chenbenyi@hotmail.com>
2021-03-28 01:13:49 +0800

    The second commit of coursework

--- cwk1/core/src/main/java/comp1721/cwk1/CaseRecord.java
+++ cwk1/core/src/main/java/comp1721/cwk1/CaseRecord.java
@@ -1,11 +1,31 @@
 package comp1721.cwk1;
+import java.time.LocalDate;
 
 public class CaseRecord {
   // TODO: Write stub for constructor
-
+ private LocalDate date;
+ private int staffCases;
+ private int studentCases;
+ private int otherCases;
   // TODO: Write stubs for four getter methods
-
+ public LocalDate getDate(){
+   return date;
+ }
+ public int getStaffCases(){
+   return staffCases;
+ }
+ public int getStudentCases(){
+   return studentCases;
+ }
+ public int getOtherCases(){
+   return otherCases;
+ }
   // TODO: Write stub for totalCases()
-  
+ public int totalCases(){
+   return 0;
+ }
   // TODO: Write stub for toString()
+ public String toString(){
+   return null;
+ }
 }
--- cwk1/core/src/main/java/comp1721/cwk1/CovidDataset.java
+++ cwk1/core/src/main/java/comp1721/cwk1/CovidDataset.java
@@ -2,14 +2,27 @@ package comp1721.cwk1;
 
 public class CovidDataset {
   // TODO: Write stub for size()
-
+  public int size(){
+    return 0;
+  }
   // TODO: Write stub for getRecord()
-
+  public CaseRecord getRecord(index:int){
+    return null;
+  }
   // TODO: Write stub for addRecord()
+  public addRecord(rec:CaseRecord){
 
+  }
   // TODO: Write stub for dailyCasesOn()
-
+  public CaseRecord dailyCasesOn(day:LocalDate){
+    return null;
+  }
   // TODO: Write stub for readDailyCases()
-
+  public readDailyCases(filename:String){
+    
+  }
   // TODO: Write stub for writeActiveCases()
+  public writeActiveCases(filename:String){
+    
+  }
 }
